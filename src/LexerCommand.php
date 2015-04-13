a75f47efad212289d431d7d6fe18831653550b8d
evtushenko <alex.the.mrix@gmail.com>
2015-04-14 00:34:30 +0600

    Add lexer command.

---
+++ src/LexerCommand.php
@@ -0,0 +1,70 @@
+<?php
+namespace Mrix\Rql\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
+use Mrix\Rql\Parser\Lexer;
+
+/**
+ */
+class LexerCommand extends Command
+{
+    /**
+     * @inheritdoc
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('rql:lexer')
+            ->setDescription('RQL lexer command')
+            ->addArgument(
+                'rql',
+                InputArgument::REQUIRED,
+                'RQL query'
+            );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $rql = $input->getArgument('rql');
+
+        $style = new OutputFormatterStyle('black', 'yellow');
+        $output->getFormatter()->setStyle('token', $style);
+
+        $lexer = new Lexer();
+        $tokenStream = $lexer->tokenize($rql);
+
+        $table  = new Table($output);
+        $table->setHeaders([
+            'Token',
+            'Type',
+            'Expression',
+        ]);
+        while (!$tokenStream->isEnd()) {
+            $token = $tokenStream->getCurrent();
+            $next = $tokenStream->lookAhead();
+
+            $table->addRow([
+                'token' => $token->getValue(),
+                'type'  => $token->getName(),
+                'text'  => implode('', [
+                    substr($rql, 0, $token->getPosition()),
+                    '<token>',
+                    substr($rql, $token->getPosition(), $next->getPosition() - $token->getPosition()),
+                    '</token>',
+                    substr($rql, $next->getPosition()),
+                ]),
+            ]);
+
+            $tokenStream->next();
+        }
+        $table->render();
+    }
+}
