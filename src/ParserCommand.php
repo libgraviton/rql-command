37aa4a91ae313ad9c9719edba8e400c0b8a1a78e
evtushenko <alex.the.mrix@gmail.com>
2015-04-14 00:34:30 +0600

    Add parser command.

---
+++ src/ParserCommand.php
@@ -0,0 +1,55 @@
+<?php
+namespace Mrix\Rql\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\VarDumper\Cloner\VarCloner;
+use Symfony\Component\VarDumper\Dumper\CliDumper;
+use Mrix\Rql\Parser\Lexer;
+use Mrix\Rql\Parser\Parser;
+use Mrix\Rql\Parser\TokenParser;
+
+/**
+ */
+class ParserCommand extends Command
+{
+    /**
+     * @inheritdoc
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('rql:parser')
+            ->setDescription('RQL parser command')
+            ->addArgument(
+                'rql',
+                InputArgument::REQUIRED,
+                'RQL query'
+            );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $lexer = new Lexer();
+        $tokenStream = $lexer->tokenize($input->getArgument('rql'));
+
+        $parser = new Parser();
+        $parser->addTokenParser(new TokenParser\SelectTokenParser());
+        $parser->addTokenParser(new TokenParser\QueryTokenParser());
+        $parser->addTokenParser(new TokenParser\SortTokenParser());
+        $parser->addTokenParser(new TokenParser\LimitTokenParser());
+
+        $dumper = new CliDumper();
+        $cloner = new VarCloner();
+        $dumper->dump($cloner->cloneVar($parser->parse($tokenStream)), function ($line, $depth) use ($output) {
+            if ($depth >= 0) {
+                $output->writeln(str_repeat('  ', $depth) . $line);
+            }
+        });
+    }
+}
